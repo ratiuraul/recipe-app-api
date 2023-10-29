b583892ccc245190652a935fbf47bc9963e26847
Ratiu Raul <ratiu.raul13@gmail.com>
2023-10-29 14:28:41 +0200

    modification not seen by git diff

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -12,6 +12,7 @@ class ModelTests(TestCase):
         """Test creating a user with an email is succesfull"""
         email = 'test@example.com'
         password = 'testpass123'
+
         user = get_user_model().objects.create_user(email=email, password=password)
 
         self.assertEqual(user.email, email)
