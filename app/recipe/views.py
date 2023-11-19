e91ad78f1c832d31aa7b8b51c615337b99f020f5
Ratiu Raul <ratiu.raul13@gmail.com>
2023-11-19 17:28:22 +0200

    fix detail recipe view

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -24,7 +24,7 @@ class RecipeViewSet(viewsets.ModelViewSet):
 
     def get_serializer_class(self):
         """Return the serializer class for request."""
-        if self.action in ('retrieve', 'update', 'partial_update', 'destroy'):
-            return serializers.RecipeDetailSerializer
+        if self.action == 'list':
+            return serializers.RecipeSerializer
 
         return self.serializer_class
