847dbba1e9cbdd8adaa94859ea56f778c84c24e1
Raul <ratiuraul@github.com>
2024-09-16 07:41:37 +0300

    configure env vars and docker compose for deploy

--- app/recipe/tests/test_ingredients_api.py
+++ app/recipe/tests/test_ingredients_api.py
@@ -118,7 +118,7 @@ class PrivateIngredientsApiTests(TestCase):
         self.assertIn(s1.data, res.data)
         self.assertNotIn(s2.data, res.data)
 
-    def test_filtered_ingredients_unqire(self):
+    def test_filtered_ingredients_unique(self):
         """Test filtered ingredients returns a unique list."""
         ing = Ingredient.objects.create(user=self.user, name="Eggs")
         Ingredient.objects.create(user=self.user, name="Lentils")
