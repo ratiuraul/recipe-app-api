acb618075b007067b5711db9a23d82b8a094e755
Raul <ratiuraul@github.com>
2024-09-18 07:56:26 +0300

    added health check api

---
+++ app/core/views.py
@@ -0,0 +1,12 @@
+"""
+Core vires for app.
+"""
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+
+@api_view(["GET"])
+def health_check(request):
+    """Resturns succesfull response."""
+    return Response({'healthy': True})
